460db0957a78971b1ad6c7e540a3111a6ef4835f
navrotskyj <navrotskyj@gmail.com>
2021-06-17 18:16:25 +0300

    fixed call_answer_sec

--- queue/schema.go
+++ queue/schema.go
@@ -78,9 +78,15 @@ func (qm *QueueManager) AfterDistributeSchema(queue *BaseQueue, att *Attempt, ca
 
 	//vars := make(map[string]string)
 
+	// fixme
+	var ansSec = 0
+	if call.AcceptAt() > 0 {
+		ansSec = int((call.HangupAt() - call.AcceptAt()) / 1000)
+	}
+
 	vars := map[string]string{
 		"call_bill_sec":   fmt.Sprintf("%d", call.BillSeconds()),
-		"call_answer_sec": fmt.Sprintf("%d", int((call.HangupAt()-call.AcceptAt())/1000)), //todo
+		"call_answer_sec": fmt.Sprintf("%d", ansSec),
 		"call_duration":   fmt.Sprintf("%d", call.DurationSeconds()),
 		"call_cause":      call.HangupCause(),
 		"call_sip_code":   fmt.Sprintf("%d", call.HangupCauseCode()),
