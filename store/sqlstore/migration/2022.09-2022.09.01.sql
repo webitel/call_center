805c31abe10b8c1a3eab39d4dedb7a156ac91a66
Igor Navrotskyj <navrotskyj@gmail.com>
2022-11-28 11:30:39 +0200

    updated sql schema

--- store/sqlstore/migration/2022.09-2022.09.01.sql
+++ store/sqlstore/migration/2022.09-2022.09.01.sql
@@ -419,3 +419,169 @@ BEGIN
 
 END;
 $$;
+
+
+
+
+--
+-- Name: cc_distribute_direct_member_to_queue(character varying, bigint, integer, bigint); Type: FUNCTION; Schema: call_center; Owner: -
+--
+
+CREATE or replace FUNCTION call_center.cc_distribute_direct_member_to_queue(_node_name character varying, _member_id bigint, _communication_id integer, _agent_id bigint) RETURNS TABLE(id bigint, member_id bigint, result character varying, queue_id integer, queue_updated_at bigint, queue_count integer, queue_active_count integer, queue_waiting_count integer, resource_id integer, resource_updated_at bigint, gateway_updated_at bigint, destination jsonb, variables jsonb, name character varying, member_call_id character varying, agent_id bigint, agent_updated_at bigint, team_updated_at bigint, seq integer)
+    LANGUAGE plpgsql
+AS $$
+declare
+    _weight      int4;
+    _destination jsonb;
+BEGIN
+
+    return query with attempts as (
+        insert into call_center.cc_member_attempt (state, queue_id, member_id, destination, node_id, agent_id, resource_id,
+                                                   bucket_id, seq, team_id, domain_id)
+            select 1,
+                   m.queue_id,
+                   m.id,
+                   m.communications -> (_communication_id::int2),
+                   _node_name,
+                   _agent_id,
+                   r.resource_id,
+                   m.bucket_id,
+                   m.attempts + 1,
+                   q.team_id,
+                   q.domain_id
+            from call_center.cc_member m
+                     inner join call_center.cc_queue q on q.id = m.queue_id
+                     inner join lateral (
+                select (t::call_center.cc_sys_distribute_type).resource_id
+                from call_center.cc_sys_queue_distribute_resources r,
+                     unnest(r.types) t
+                where r.queue_id = m.queue_id
+                  and (t::call_center.cc_sys_distribute_type).type_id =
+                      (m.communications -> (_communication_id::int2) -> 'type' -> 'id')::int4
+                limit 1
+                ) r on true
+                     left join call_center.cc_outbound_resource cor on cor.id = r.resource_id
+            where m.id = _member_id
+              and m.communications -> (_communication_id::int2) notnull
+              and not exists(select 1 from call_center.cc_member_attempt ma where ma.member_id = _member_id)
+            returning call_center.cc_member_attempt.*
+    )
+                 select a.id,
+                        a.member_id,
+                        null::varchar          result,
+                        a.queue_id,
+                        cq.updated_at as       queue_updated_at,
+                        0::integer             queue_count,
+                        0::integer             queue_active_count,
+                        0::integer             queue_waiting_count,
+                        a.resource_id::integer resource_id,
+                        r.updated_at::bigint   resource_updated_at,
+                        null::bigint           gateway_updated_at,
+                        a.destination          destination,
+                        cm.variables,
+                        cm.name,
+                        null::varchar,
+                        a.agent_id::bigint     agent_id,
+                        ag.updated_at::bigint  agent_updated_at,
+                        t.updated_at::bigint   team_updated_at,
+                        a.seq::int seq
+                 from attempts a
+                          left join call_center.cc_member cm on a.member_id = cm.id
+                          inner join call_center.cc_queue cq on a.queue_id = cq.id
+                          left join call_center.cc_outbound_resource r on r.id = a.resource_id
+                          left join call_center.cc_agent ag on ag.id = a.agent_id
+                          inner join call_center.cc_team t on t.id = ag.team_id;
+
+    --raise notice '%', _attempt_id;
+
+END;
+$$;
+
+
+
+--
+-- Name: cc_set_active_members(character varying); Type: FUNCTION; Schema: call_center; Owner: -
+--
+
+CREATE or replace FUNCTION call_center.cc_set_active_members(node character varying) RETURNS TABLE(id bigint, member_id bigint, result character varying, queue_id integer, queue_updated_at bigint, queue_count integer, queue_active_count integer, queue_waiting_count integer, resource_id integer, resource_updated_at bigint, gateway_updated_at bigint, destination jsonb, variables jsonb, name character varying, member_call_id character varying, agent_id integer, agent_updated_at bigint, team_updated_at bigint, list_communication_id bigint, seq integer)
+    LANGUAGE plpgsql
+AS $$
+BEGIN
+    return query update call_center.cc_member_attempt a
+        set state = case when c.queue_type = 4 then 'offering' else 'waiting' end
+            ,node_id = node
+            ,last_state_change = now()
+            ,list_communication_id = lc.id
+            ,seq = c.attempts + 1
+            ,waiting_other_numbers = c.waiting_other_numbers
+        from (
+            select c.id,
+                   cq.updated_at                                            as queue_updated_at,
+                   r.updated_at                                             as resource_updated_at,
+                   call_center.cc_view_timestamp(gw.updated_at)             as gateway_updated_at,
+                   c.destination                                            as destination,
+                   cm.variables                                             as variables,
+                   cm.name                                                  as member_name,
+                   c.state                                                  as state,
+                   cqs.member_count                                         as queue_cnt,
+                   0                                                        as queue_active_cnt,
+                   cqs.member_waiting                                       as queue_waiting_cnt,
+                   (ca.updated_at - extract(epoch from u.updated_at))::int8 as agent_updated_at,
+                   tm.updated_at                                            as team_updated_at,
+                   cq.dnc_list_id,
+                   cm.attempts,
+                   x.cnt                                                    as waiting_other_numbers,
+                   cq.type                                                  as queue_type
+            from call_center.cc_member_attempt c
+                     inner join call_center.cc_member cm on c.member_id = cm.id
+                     left join lateral (
+                select count(*) cnt
+                from jsonb_array_elements(cm.communications) WITH ORDINALITY AS x(c, n)
+                where coalesce((x.c -> 'stop_at')::int8, 0) < 1
+                  and x.n != (c.communication_idx + 1)
+                ) x on c.member_id notnull
+                     inner join call_center.cc_queue cq on cm.queue_id = cq.id
+                     left join call_center.cc_team tm on tm.id = cq.team_id
+                     left join call_center.cc_outbound_resource r on r.id = c.resource_id
+                     left join directory.sip_gateway gw on gw.id = r.gateway_id
+                     left join call_center.cc_agent ca on c.agent_id = ca.id
+                     left join call_center.cc_queue_statistics cqs on cq.id = cqs.queue_id
+                     left join directory.wbt_user u on u.id = ca.user_id
+            where c.state = 'idle'
+              and c.leaving_at isnull
+            order by cq.priority desc, c.weight desc
+                for update of c, cm, cq skip locked
+        ) c
+            left join call_center.cc_list_communications lc on lc.list_id = c.dnc_list_id and
+                                                               lc.number = c.destination ->> 'destination'
+        where a.id = c.id --and node = 'call_center-igor'
+        returning
+            a.id::bigint as id,
+            a.member_id::bigint as member_id,
+            a.result as result,
+            a.queue_id::int as qeueue_id,
+            c.queue_updated_at::bigint as queue_updated_at,
+            c.queue_cnt::int,
+            c.queue_active_cnt::int,
+            c.queue_waiting_cnt::int,
+            a.resource_id::int as resource_id,
+            c.resource_updated_at::bigint as resource_updated_at,
+            c.gateway_updated_at::bigint as gateway_updated_at,
+            c.destination,
+            c.variables ,
+            c.member_name,
+            a.member_call_id,
+            a.agent_id,
+            c.agent_updated_at,
+            c.team_updated_at,
+            a.list_communication_id,
+            a.seq;
+END;
+$$;
+
+--
+-- Name: cc_member_queue_id_created_at; Type: INDEX; Schema: call_center; Owner: -
+--
+
+CREATE INDEX cc_member_queue_id_created_at ON call_center.cc_member USING btree (queue_id, created_at DESC);
+
