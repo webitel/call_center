9dd95309d6af4ea6fdeefd8aaffd9afbc1c16f9b
i.navrotskyj <i.navrotskyj@gmail.com>
2024-05-03 13:57:53 +0300

    DEV-4103

--- queue/queue_call.go
+++ queue/queue_call.go
@@ -47,7 +47,7 @@ func IsHuman(call call_manager.Call, amd *model.QueueAmdSettings) bool {
 	if amd.Ai {
 		aiAmd := call.AiResult()
 		answered := call.Answered()
-		if aiAmd.Error != "" || aiAmd.Result == "undefined" {
+		if aiAmd.Error != "" /*|| aiAmd.Result == "undefined"*/ {
 			return answered // TODO ? DEV-3338
 		}
 		for _, v := range amd.PositiveTags {
