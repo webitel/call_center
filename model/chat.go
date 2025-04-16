ff2b8ba8eabc522abb7cbf109de123d982e36059
i.navrotskyj <i.navrotskyj@gmail.com>
2025-04-16 12:26:40 +0300

    WTEL-4778

--- model/chat.go
+++ model/chat.go
@@ -1,14 +1,14 @@
 package model
 
 type InboundChatQueue struct {
-	AttemptId      int64             `json:"attempt_id" db:"attempt_id"`
-	QueueId        int               `json:"queue_id" db:"queue_id"`
-	QueueUpdatedAt int64             `json:"queue_updated_at" db:"queue_updated_at"`
-	Destination    []byte            `json:"destination" db:"destination"`
-	Variables      map[string]string `json:"variables" db:"variables"`
-	Name           string            `json:"name" db:"name"`
-	TeamUpdatedAt  *int64            `json:"team_updated_at" db:"team_updated_at"`
-	//ListCommunicationId *int64 `json:"list_communication_id" db:"list_communication_id"`
+	AttemptId           int64             `json:"attempt_id" db:"attempt_id"`
+	QueueId             int               `json:"queue_id" db:"queue_id"`
+	QueueUpdatedAt      int64             `json:"queue_updated_at" db:"queue_updated_at"`
+	Destination         []byte            `json:"destination" db:"destination"`
+	Variables           map[string]string `json:"variables" db:"variables"`
+	Name                string            `json:"name" db:"name"`
+	TeamUpdatedAt       *int64            `json:"team_updated_at" db:"team_updated_at"`
+	ListCommunicationId *int64            `json:"list_communication_id" db:"list_communication_id"`
 
 	ConversationId        string `json:"conversation_id" db:"conversation_id"`
 	ConversationCreatedAt int64  `json:"conversation_created_at" db:"conversation_created_at"`
