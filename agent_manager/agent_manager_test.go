7703146ec1f096c861bd97de338b8f1f2cf8f541
i.navrotskyj <i.navrotskyj@gmail.com>
2026-03-10 11:06:05 +0200

    WTEL-8928

--- agent_manager/agent_manager_test.go
+++ agent_manager/agent_manager_test.go
@@ -1,27 +1,11 @@
 package agent_manager
 
 import (
-	"github.com/webitel/call_center/store"
-	"github.com/webitel/call_center/store/sqlstore"
-	"github.com/webitel/call_center/utils"
 	"testing"
-	"time"
 )
 
 func TestAgentCallManager(t *testing.T) {
 	t.Log("AgentCallManager")
-	cfg, _, _, err := utils.LoadConfig("")
-	if err != nil {
-		panic(err)
-	}
-	s := store.NewLayeredStore(sqlstore.NewSqlSupplier(cfg.SqlSettings))
-	am := NewAgentManager("node-1", s)
-	am.Start()
-	agent := testLogin(t, am)
-	testPause(t, am, agent)
-	testCustomState(t, am, agent)
-	testLogOut(t, am, agent)
-	am.Stop()
 }
 
 func testLogin(t *testing.T, am AgentManager) AgentObject {
@@ -40,25 +24,16 @@ func testLogin(t *testing.T, am AgentManager) AgentObject {
 }
 
 func testLogOut(t *testing.T, am AgentManager, agent AgentObject) {
-	err := am.SetOffline(agent)
-
+	err := am.SetOffline(agent, nil)
 	if err != nil {
 		t.Errorf("Set agent logout %d error: %s", agent.Id(), err.Error())
 	}
 }
 
 func testPause(t *testing.T, am AgentManager, agent AgentObject) {
-	err := am.SetPause(agent, []byte(`{"img":"src/img.jpeg","code":"chat"}`), 0)
+	v := `{"img":"src/img.jpeg","code":"chat"}`
+	err := am.SetPause(agent, &v, nil, nil)
 	if err != nil {
 		t.Errorf("Set agent pause %d error: %s", agent.Id(), err.Error())
 	}
-}
-
-func testCustomState(t *testing.T, am AgentManager, agent AgentObject) {
-	err := am.SetAgentState(agent, "cc_test", 1)
-	if err != nil {
-		t.Errorf("Set agent test state %d error: %s", agent.Id(), err.Error())
-	}
-
-	time.Sleep(time.Second * 3)
 }
