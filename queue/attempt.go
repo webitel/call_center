e176b610793f921b5af9e5fb749149626a827ed5
i.navrotskyj <i.navrotskyj@gmail.com>
2026-09-07 12:58:57 +0300

    feat(attempt): add queue name to exported schema variables

--- queue/attempt.go
+++ queue/attempt.go
@@ -484,6 +484,7 @@ func (a *Attempt) ExportSchemaVariables() map[string]string {
 		if a.queue.Processing() {
 			res["use_processing"] = "true"
 		}
+        res["queue_name"] = a.queue.Name()
 	}
 
 	if a.agent != nil {
